3b7a1b1edbdcf37dae382db235364d2ebe0707d6
arnavanand2005 <arnavanand2005@gmail.com>
2025-06-20 18:12:01 +0530

    api-initialised

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React ,{useState}from 'react'
+import { fetchCityWeather } from './service/api'
 
 const App = () => {
 
   const[city,setCity]=useState("")
+  const [data,setData]=useState(null)
+  const [loading,setLoading]=useState(false)
+  const [error,setError]=useState(null)
 
   function handleCityChange(e){
     setCity(e.target.value)
   }
+
+  async function searchcityweather(){
+    setLoading(true)
+    setError(null)
+    try {
+      const weatherData = await fetchCityWeather(city)
+      setData(weatherData)
+    }
+    catch (error) {
+      setError(error.message)
+      }
+    finally{
+      setLoading(false)
+    }
+  }
+
+
   return (
     <div className='full-weather-form-container'>
       <h1 className='main-heading'>
@@ -19,12 +40,29 @@ const App = () => {
          onChange={handleCityChange} 
          value={city}/>
 
-         <button className='search-btn'>
-          Search
+         <button className='search-btn'
+         onClick={searchcityweather}
+         disabled={loading}>
+          {loading ? 'Loading...' : 'Search'}
          </button>
-
-         <p>{city}</p>
       </div>
+
+      {error && <p className='error-message'>⚠️ {error}</p>}
+      {data && (
+      <div className="weather-city-display">
+        <h3 className='city-name'>{data.name}</h3>
+        <img className='main-weather-image' 
+            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
+            alt="Weather icon" 
+        />        
+        <p className='temperature-condition'>{Math.round(data.main.temp)}</p>
+        <p className='weather-condition'>{data.weather[0].description}</p>
+        <div className="essential-weather-conditions">
+          <p className='wind-conditon'>Wind : {data.wind.speed} m/s</p>
+          <p className='humidity-condition'>Humidity : {data.main.humidity} %</p>
+        </div>
+      </div>
+    )}
     </div>
   )
 }
---
+++ src/service/api.js
@@ -0,0 +1,31 @@
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
+const API_KEY = import.meta.env.VITE_API_KEY;   
+
+async function fetchCityWeather(city) {
+  if (!city.trim()) {
+    throw new Error("City name cannot be empty");
+  }
+
+  try {
+    const response = await fetch(
+        `${BASE_URL}weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`
+      );
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error("City not found");
+      } else if (response.status === 401) {
+        throw new Error("Invalid API key");
+      } else {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Weather API Error:", error);
+    throw error; 
+  }
+}
+
+export { fetchCityWeather };
