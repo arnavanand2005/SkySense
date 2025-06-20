933792df02fc3cea5821eebbb61db6b13366902f
arnavanand2005 <arnavanand2005@gmail.com>
2025-06-20 17:32:50 +0530

    input-initialisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React ,{useState}from 'react'
 
 const App = () => {
+
+  const[city,setCity]=useState("")
+
+  function handleCityChange(e){
+    setCity(e.target.value)
+  }
   return (
-    <div>
-      
+    <div className='full-weather-form-container'>
+      <h1 className='main-heading'>
+        SkySense - Weather App
+      </h1>
+      <div className="input-container">
+        <input type="text"
+         className='city-input'
+         placeholder='Enter City Name...'
+         onChange={handleCityChange} 
+         value={city}/>
+
+         <button className='search-btn'>
+          Search
+         </button>
+
+         <p>{city}</p>
+      </div>
     </div>
   )
 }
